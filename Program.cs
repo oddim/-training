d5265ec6b6ba3f2c488ee4e47400cb99209756c9
oddim <allayarovdim@gmail.com>
2025-04-27 19:54:32 +0500

    Work with strings

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza
+{
+    internal class StringsWork
+    {
+        static void Main(string[] args)
+        {
+            string userName, userAge, userProfession, userSex;
+            Console.WriteLine("Заполните анкету");
+            Console.Write("Ваше имя и фамилию: ");
+            userName = Console.ReadLine();
+            Console.Write("Ваш возраст: ");
+            userAge = Console.ReadLine();
+            Console.Write("Ваш пол: ");
+            userSex = Console.ReadLine();
+            Console.Write("Ваша профессия: ");
+            userProfession = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {userName}. Вам {userAge} и вы {userSex}. Ваша профессия - {userProfession}");
+            Console.ReadKey();
+        }
+    }
+}
