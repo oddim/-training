7cf7e29d87b330cbadc5159ee40bb2a1e42c3404
oddim <allayarovdim@gmail.com>
2025-04-27 16:24:32 +0500

    Add files via upload

---
+++ variable types.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool theBool = true;
+            byte theByte = 24;
+            int theInt = 219;
+            uint theUint = 3040100;
+            float theFloat = -5.9f;
+            char theChar = 'q';
+            double theDouble = 10.5;
+            string theString = "строка" ;
+            short theShort = -1324;
+            ushort theUshort = 15234;
+
+        }
+    }
+}
