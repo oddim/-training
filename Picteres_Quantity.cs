fe0079129312a01e0b4535ca8d4110705189a868
oddim <allayarovdim@gmail.com>
2025-04-28 23:01:21 +0500

    Picteres_Quantity

---
+++ Picteres_Quantity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cslight
+{
+    internal class Picteres_Quantity
+    {
+        static void Main(string[] args)
+        {
+            int quantityPic = 52;
+            int series1Pic = 3;
+            int showSeriesPic;
+            int extraShowPic;
+            showSeriesPic = quantityPic / series1Pic;
+            extraShowPic = quantityPic % series1Pic;
+            Console.WriteLine($"Вы можете вывести рядов картинок {showSeriesPic}, {extraShowPic} уже сверх меры ");
+
+        }
+    }
+}
