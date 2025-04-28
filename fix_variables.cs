f77a99e3f73c7ee777ea6a3c3aff397382637a3c
oddim <allayarovdim@gmail.com>
2025-04-28 23:53:02 +0500

    fix_variables

---
+++ fix_variables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fix_variables
+{
+    internal class fix_variables
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c;
+            Console.WriteLine($"a = {a}, b = {b}");
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine($"a = {a}, b = {b}");
+        }
+    }
+}
