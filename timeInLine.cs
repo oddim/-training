a36f5aa64b086cadaac93b1fa965e0d36d2316fb
oddim <allayarovdim@gmail.com>
2025-05-02 20:19:33 +0500

    timeInLine

---
+++ timeInLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class timeInLine
+    {
+        static void Main(string[] args)
+        {
+            int timePersonClinic = 10;
+            Console.Write("Сколько людей в очереди? :");
+            int lineAmountPerson = Convert.ToInt32(Console.ReadLine());
+            int hourWainting = timePersonClinic * lineAmountPerson / 60;
+            int minutesWaiting = timePersonClinic*lineAmountPerson - hourWainting*60;
+            Console.WriteLine($"Вы будете стоять в очереди {hourWainting} часа и {minutesWaiting} минут");
+        }
+    }
+}
