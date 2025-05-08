6a2cd664f3ad521f9ac327bfef799d5fb7737785
oddim <allayarovdim@gmail.com>
2025-05-08 14:26:28 +0500

    Cycles

---
+++ Cycles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Cycles
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество раз вывода сообщения: ");
+            int amountWriting =  Convert.ToInt32(Console.ReadLine());
+            
+            while (amountWriting > 0)
+            {
+                Console.WriteLine("Message");
+                amountWriting--;
+            }
+        }
+    }
+}
