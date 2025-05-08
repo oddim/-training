f303ce548ce7748ee54ca0500b494e34ac416a19
oddim <allayarovdim@gmail.com>
2025-05-08 15:22:36 +0500

    SetNumbers

---
+++ SetNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class SetNumbers
+    {
+        static void Main(string[] args)
+        {
+            int beginingNumber = 5;
+            int stepNumber = 7;
+            int maxNumber = 789;
+
+            for (int i = 0; beginingNumber <= maxNumber; i++)
+            {
+                Console.WriteLine(beginingNumber);
+                beginingNumber = beginingNumber + stepNumber;
+            }
+        }
+    }
+}
