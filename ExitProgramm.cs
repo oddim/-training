d6cd16144f37ef11db01924bc3a6002a3687f579
oddim <allayarovdim@gmail.com>
2025-05-08 14:45:41 +0500

    ExitProgramm

---
+++ ExitProgramm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class ExitProgramm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите exit для выхода из программы");
+
+            while (true)
+            {
+                string writingExit = Console.ReadLine();
+
+                if (writingExit == "exit")
+                {
+                    Console.WriteLine("Вы вышли из программы");
+                    break;
+                }
+
+                Console.WriteLine("Программа всё еще работает");
+            }
+        }
+    }
+}
