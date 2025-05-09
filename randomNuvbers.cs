0d209ab9172eef682f2468d93e6e4eeba7669c35
oddim <allayarovdim@gmail.com>
2025-05-09 12:53:04 +0500

    randomNumbers

---
+++ randomNuvbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sumNumbers = 0;
+            int multipleNumber3 = 3;
+            int multipleNumber5 = 5;
+            int maxNumber = 100;
+            Random rand = new Random();
+
+            number = rand.Next(0,maxNumber);
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % multipleNumber3 == 0 || i % multipleNumber5 == 0)
+                {
+                    sumNumbers = sumNumbers + i;
+                }
+            }
+
+            Console.WriteLine(sumNumbers);
+        }
+    }
+}
