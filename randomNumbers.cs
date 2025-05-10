43b424c73d5231edc53ac1520a8bf5e4e13cfbcb
oddim <allayarovdim@gmail.com>
2025-05-10 13:20:02 +0500

    Update randomNumbers.cs

--- randomNumbers.cs
+++ randomNumbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,21 +12,22 @@ namespace ConsoleApp5
         {
             int number;
             int sumNumbers = 0;
-            int multipleNumber3 = 3;
-            int multipleNumber5 = 5;
+            int firstDivisor = 3;
+            int secondDivisor = 5;
             int maxNumber = 100;
-            Random rand = new Random();
 
+            Random rand = new Random();
             number = rand.Next(0,maxNumber);
+
             for (int i = 0; i <= number; i++)
             {
-                if (i % multipleNumber3 == 0 || i % multipleNumber5 == 0)
+                if (i % firstDivisor == 0 || i % secondDivisor == 0)
                 {
                     sumNumbers = sumNumbers + i;
                 }
             }
 
-            Console.WriteLine(sumNumbers);
+            Console.WriteLine($"Рандомное число: {sumNumbers}");
         }
     }
 }
